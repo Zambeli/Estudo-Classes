5079378b1dfcbcc7fd69d4b5b0e80d7fc42a77a7
Zambeli <zambelidev@gmail.com>
2024-06-05 20:58:02 -0300

    projeto finalizado

--- src/index.js
+++ src/index.js
@@ -3,12 +3,19 @@ import { Arqueiro } from "./modules/arqueiro.js";
 import { Personagem } from "./modules/personagem.js";
 import { PersonagemView } from "./components/personagem-view.js";
 import { ArqueiroMago } from "./modules/arqueiro-mago.js";
+import { Guerreiro } from "./modules/guerreiro.js";
 
 const magoAntonio = new Mago('Antonio',4,'Fogo',4,3)
 const magaJulia = new Mago('Julia',8,'Gelo',7,10)
 const arqueiroGustavo = new Arqueiro('Gustavo',8,10)
 const arqueiroMagoChico = new ArqueiroMago('Chico',7,10,'ar',4,8)
+const guerreiroCarlos = new Guerreiro('Carlos',7)
 
-const personagens = [magoAntonio,magaJulia,arqueiroGustavo,arqueiroMagoChico]
+
+const personagens = [magoAntonio,magaJulia,arqueiroGustavo,arqueiroMagoChico,guerreiroCarlos]
 
 new PersonagemView(personagens).render()
+
+console.log(Personagem.verificarVencedor(arqueiroMagoChico, arqueiroGustavo));
+
+console.log(arqueiroGustavo());
